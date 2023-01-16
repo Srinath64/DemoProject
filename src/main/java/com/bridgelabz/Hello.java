fa195ac8a8ddf9c2723d4ba6a2aaf83050e8fc0e
Srinath S <connect2srinath06@gmail.com>
2023-01-16 17:01:22 +0530

    Added : Github !!!

--- src/main/java/com/bridgelabz/Hello.java
+++ src/main/java/com/bridgelabz/Hello.java
@@ -3,8 +3,8 @@ package com.bridgelabz;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("Github");
     }
 }
 
-//.gitignore
 
