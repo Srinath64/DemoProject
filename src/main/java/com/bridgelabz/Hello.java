5190ee2ad54d208a775a5c62de4947ca9c7b92a9
Srinath S <connect2srinath06@gmail.com>
2023-01-18 17:16:58 +0530

    Added

--- src/main/java/com/bridgelabz/Hello.java
+++ src/main/java/com/bridgelabz/Hello.java
@@ -1,8 +1,18 @@
 package com.bridgelabz;
 
 public class Hello {
-    public static void main(String[] args) {
-        System.out.println("Hello World");
+    public static void main(String[] args){
+        int i, s=0, c=0;
+        for(i=0;i<args.length;i++){
+            try{
+                s+=Integer.parseInt(args[i]);
+            }
+            catch(NumberFormatException ex){
+                c++;
+            }
+        }
+        System.out.println("sum of command line arguments is "+s);
+        System.out.println("Number of invalid integers are "+c);
     }
 }
 
